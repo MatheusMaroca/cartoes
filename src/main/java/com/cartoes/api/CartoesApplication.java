8d994685bcc78d4a82b56a188f2eec99a9b9a711
MatheusMaroca <matheusbarrosomaroca@hotmail.com>
2020-08-23 22:44:47 -0300

    v2 mysql

--- src/main/java/com/cartoes/api/CartoesApplication.java
+++ src/main/java/com/cartoes/api/CartoesApplication.java
@@ -1,4 +1,4 @@
-package com.cartoes.ap;
+package com.cartoes.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/cartoes/api/repositories/TransacaoRepository.java
@@ -0,0 +1,5 @@
+package com.cartoes.api.repositories;
+
+public interface TransacaoRepository {
+
+}

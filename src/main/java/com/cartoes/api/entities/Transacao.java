d05d8f46da4ec850c1151886a850faf51c228327
MatheusMaroca <matheusbarrosomaroca@hotmail.com>
2020-09-08 20:46:59 -0300

    Atividade 2 em andamento

--- src/main/java/com/cartoes/api/entities/Transacao.java
+++ src/main/java/com/cartoes/api/entities/Transacao.java
@@ -2,7 +2,6 @@ package com.cartoes.api.entities;
 
 import java.io.Serializable;
 import java.util.Date;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -15,42 +14,44 @@ import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 @Table(name = "transacao")
 public class Transacao implements Serializable {
 	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
-	
+
 	@Column(name = "data_Transacao", nullable = false)
 	private Date dataTransacao;
 
-	@Column(name = "CNPJ", nullable = false, length = 14, unique = true)
-	private String CNPJ;
+	@Column(name = "cnpj", nullable = false, length = 14)
+	private String cnpj;
 
 	@Column(name = "valor", nullable = false)
-	private Double valor;
+	private double valor;
 
 	@Column(name = "qdt_Parcelas", nullable = false)
 	private int qdtParcelas;
-	
+
 	@Column(name = "juros", nullable = false)
-	private Double juros;
-	
+	private double juros;
+
 	@JsonBackReference
-	@ManyToOne(fetch = FetchType.EAGER)
-	private Cartao cartao;
-	
+	@ManyToOne (fetch = FetchType.EAGER)
+	private  Cartao cartao;
+
 	public int getId() {
 		return id;
 	}
-	
+
 	public void setId(int id) {
 		this.id = id;
 	}
-	
+
 	public Date getDataTransacao() {
 		return dataTransacao;
 	}
@@ -58,23 +59,23 @@ public class Transacao implements Serializable {
 	public void setDataTransacao(Date dataTransacao) {
 		this.dataTransacao = dataTransacao;
 	}
-	
+
 	public String getCnpj() {
-		return CNPJ;
+		return cnpj;
 	}
 
-	public void setCnpj(String CNPJ) {
-		this.CNPJ = CNPJ;
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
 	}
-	
-	public Double getValor() {
+
+	public double getValor() {
 		return valor;
 	}
 
-	public void setValor(Double valor) {
+	public void setValor(double valor) {
 		this.valor = valor;
 	}
-	
+
 	public int getQdtParcelas() {
 		return qdtParcelas;
 	}
@@ -82,37 +83,37 @@ public class Transacao implements Serializable {
 	public void setQdtParcelas(int qdtParcelas) {
 		this.qdtParcelas = qdtParcelas;
 	}
-	
+
+
 	public double getJuros() {
 		return juros;
 	}
 
-	public void setJuros(Double juros) {
+	public void setJuros(double juros) {
 		this.juros = juros;
 	}
-	
+
 	public Cartao getCartao() {
 		return cartao;
 	}
 
-	public void Cartao(Cartao cartao) {
+	public void setCartao(Cartao cartao) {
 		this.cartao = cartao;
-	}
-	
-	@PreUpdate
-	public void preUpdate() {
-		dataTransacao = new Date();
 	}
 
 	@PrePersist
 	public void prePersist() {
 		dataTransacao = new Date();
 	}
-	
 	@Override
-	public String toString() {
-		return "Transacao[" + "id=" + id + "," + "dataTransacao" + dataTransacao + "," + "CNPJ=" + CNPJ + "," + "valor=" + valor + ","
-				+ "qdtParcelas=" + qdtParcelas + "," + "juros=" + juros +  "]";
-	}
+	 public String toString() {
+		return "Transacao[" + "id=" + id + ","
+				 + "dataTransacao=" + dataTransacao + ","
+				 + "cnpj=" + cnpj + ","
+				 + "valor=" + valor + ","
+				 + "juros=" + juros + ","
+				 +" cartao=" + cartao + ","
+				 + "qdtParcelas=" + qdtParcelas + "]";
+
+	 }
 }
-	
--- src/main/java/com/cartoes/api/repositories/TransacaoRepository.java
+++ src/main/java/com/cartoes/api/repositories/TransacaoRepository.java
@@ -1,14 +1,17 @@
 package com.cartoes.api.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.cartoes.api.entities.Cartao;
 import com.cartoes.api.entities.Transacao;
 
+
 public interface TransacaoRepository extends JpaRepository<Transacao, Integer> {
-
     @Transactional(readOnly = true)
-       Cartao findByNumero(int numero);
-
+    @Query("SELECT ta FROM Transacao ta WHERE ta.cartao.numero = :cartaoNumero")
+    List<Transacao> findByCartaoNumero(@Param("cartaoNumero") String cartaoNumero);
 }
